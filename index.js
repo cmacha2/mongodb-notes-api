537841b960f3b2d1c3849243fd0ffcb4d8e20219
cmacha2 <cmacha2.08@gmail.com>
2022-06-24 15:06:02 -0400

    terminando un CRUD backend Notas

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-
+require('dotenv').config()
 const { default: mongoose } = require('mongoose')
 const app = require('./routes')
-const PORT = 3001
 
 
-const connectionString = 'mongodb+srv://machado:1Q7pDygxXkNQufCd@cluster0.xeyhm.mongodb.net/machadodb?retryWrites=true&w=majority'
+const connectionString = process.env.MONGO_DB_URL
 
 
 
 mongoose.connect(connectionString).then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server running on por ${PORT}`)
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server running on por ${process.env.PORT}`)
     })
     console.log('Database connected')
 }).catch(error => console.log(error))
 
+process.on('uncaughtException', ()=>{
+    mongoose.connection.disconnect()
+})
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,110 @@
-const express = require('express')
-const Note = require('../models/Note')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const Note = require("../models/Note");
+const cors = require("cors");
+const notFound = require("../middleware/notFound");
+const handleErrors = require("../middleware/handleErrors");
+const api = require("@serverless/cloud");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello World</h1>')
-})
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/notes', (req,res)=>{
-    const notes = Note.find({})
-    .then((result)=>res.json(result))
-    .catch(error=>console.log(error))
-})
+Sentry.init({
+  dsn: "https://877cb6c85be547bab38a51071eaa1c79@o1298315.ingest.sentry.io/6528526",
+  // or pull from params
+  // dsn: params.SENTRY_DSN,
+  // environment: params.INSTANCE_NAME,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
 
-app.post('/api/notes', (req,res)=>{
-    const {content,date,important} = req.body
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // or pull from params
+  // tracesSampleRate: parseFloat(params.SENTRY_TRACES_SAMPLE_RATE),
+});
 
-    const note = new Note({
-        content,
-        date,
-        important
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+app.get("/api/notes", (req, res, next) => {
+  Note.find({})
+    .then((result) => res.json(result))
+    .catch((error) => next(error));
+});
+
+app.post("/api/notes", (req, res, next) => {
+  const note = req.body;
+
+  if (!note.content) {
+    return res.status(400).json({ error: 'requiered "content" field missing' });
+  }
+
+  const newNote = new Note({
+    content: note.content,
+    date: new Date(),
+    important: note.important || false,
+  });
+  newNote
+    .save()
+    .then((result) => res.json(result))
+    .catch((error) => next(error));
+});
+
+app.get("/api/notes/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Note.findById(id)
+    .then((note) => {
+      return note
+        ? res.json(note)
+        : res.status(400).json({ error: "id used is malformed" }).end();
     })
-    note.save()
-    .then(result=> res.json(result))
-    .catch(error=> res.status(400).json(error))
-})
+    .catch((error) => next(error));
+});
 
+app.delete("/api/notes/:id", (req, res, next) => {
+  const { id } = req.params;
 
-module.exports = app
+  Note.findByIdAndDelete(id)
+    .then((note) => {
+      return note
+        ? res.status(204).end()
+        : res.status(400).json({ error: "id used is malformed" }).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const { id } = req.params;
+  const note = req.body;
+
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important,
+  };
+
+  Note.findByIdAndUpdate(id, newNoteInfo, { new: true }) //=> devuelve por defecto el valor que encontro no el actualizado
+    .then((noteUpdate) => {
+      res.json(noteUpdate);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(notFound);
+app.use(Sentry.Handlers.errorHandler());
+app.use(handleErrors);
+
+module.exports = app;
